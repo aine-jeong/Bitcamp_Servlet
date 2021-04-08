a1dcf0c12286f81c22f0ef18e1d28de5970088d5
aine_Jeong <mydream9612@naver.com>
2021-04-08 16:44:32 +0900

    21-04-08 Filter 적용

---
+++ WebServlet_91_Filter/WebContent/login/loginOk.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+   //1. 한글처리
+   //데이터를 받는 모든 페이지 상단에는
+   //request.setCharacterEncoding("UTF-8");
+   //데이터 받기 전에 
+   
+   //관심사(주관심 , 보조관심)
+   //데이터 받아서 화면에 출력
+   //보조관심(공통관심) : 한글처리
+   
+   //주관심, 보조(공통) 분리하자 (AOP)
+   
+   //공통관심을 분리해서 작업하도록 해주는 기능
+   //Filter 
+%>    
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+한글명:${param.kor}<br>
+영문명:${param.eng}<br>
+</body>
+</html>
--- WebServlet_91_Memo_mvc_dbcp_helper_Interface/src/kr/or/bit/controller/FrontMemoController.java
+++ WebServlet_91_Memo_mvc_dbcp_helper_Interface/src/kr/or/bit/controller/FrontMemoController.java
@@ -30,6 +30,7 @@ public class FrontMemoController extends HttpServlet {
 
     private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
+    	//요청주소의 마지막 값을 뽑아내는 작업
     	String requestURI = request.getRequestURI();
     	String contextPath = request.getContextPath();
     	String url_Command = requestURI.substring(contextPath.length());
@@ -37,7 +38,8 @@ public class FrontMemoController extends HttpServlet {
     	Action action=null;
     	ActionForward forward=null;
     	
-    	if(url_Command.equals("/MemoAdd.memo")) { //글쓰기 처리
+    	//memo.html에서 MemoAdd.memo로 전달이 들어왔을 때, 글쓰기 처리
+    	if(url_Command.equals("/MemoAdd.memo")) {
     		//UI+로직
     		action = new MemoAddService();
     		forward = action.execute(request, response);
@@ -49,13 +51,25 @@ public class FrontMemoController extends HttpServlet {
     		forward = action.execute(request, response);
     		System.out.println("MemoListService 실행");
     		
+    		
+    	//비동기의 경우 view가 필요없는뎁... ?
+    	//1. 비동기의 경우 Servlet을 따로 만든다 (요청주소에 대한 Servlet을 가지고 있는 것)
+    	//	 동기 방식은 컨트롤러 태워서 처리, 비동기는 따로 처리
+    	//2. 비동기도 컨트롤러 태우기
+    	// 	 HTML에 MemoId.memo url 넣기
+    	//	 이 아래의 코드가 비동기도 컨트롤러로 넘어오는 방법!
+    	//	 이 케이스의 경우 uservalid가 따로 만들어진다
+    	//	 UI를 미리 작업해서 클라이언트한테 날릴거면 이렇게 해도 되지만,
+    	//	 요즘은 UI는 클라이언트한테 맡기고 ONLY 데이터만 넘긴다 -> 1번 방식을 사용하는게 낫다(why? uservalid가 너무 의미없이 만들어짐)
     	}else if(url_Command.equals("/MemoId.memo")) { //비동기(ID 사용 유무)
     		//UI+로직
     		action = new MemoIdCheckService();
     		forward = action.execute(request, response);
     		
     		System.out.println("MemoIdCheckService 실행");
-    	}else if(url_Command.equals("/MemoView.memo")) { //만약 있다면 상세보기
+    		
+    	//상세보기 기능 (현재 음슴)
+    	}else if(url_Command.equals("/MemoView.memo")) { 
     		//UI 제공 ...
     		//예) /WEB-INF/views/memoview.jsp 가정
     		forward = new ActionForward();
